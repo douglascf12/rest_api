439dbd1523bc109ae16b9cdffcd9e51bd7492362
Douglas Cardoso <douglascf12@live.com>
2021-11-19 20:16:42 -0300

    segura chiquinho

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const http = require('http')
-const app = require('./app')
-const port = process.env.PORT || 3000
-const server = http.createServer(app)
-server.listen(port)
+(async() => {
+    const http = require('http')
+    const app = require('./app')
+
+    //BD
+    const db = require("./db")
+    console.log("Começou!")
+    console.log('INSERT INTO PRODUTOS')
+    await db.insertProduto({ titulo: "Berma", descricao: "Berma pesada", categoria: "Bermuda"})
+    console.log('SELECT * FROM produtos')
+    const produtos = await db.selectCustomers()
+    console.log(produtos)
+
+    //PORTA
+    const port = process.env.PORT || 3000
+    const server = http.createServer(app)
+    server.listen(port)
+})()
